84fbd13f7bb6efb7679d0339de69fe9d1653f24d
haluoz <h374726044@gmail.com>
2020-08-20 20:52:50 +0800

    100 percent complete

--- Assets/PrintAndHide.cs
+++ Assets/PrintAndHide.cs
@@ -6,15 +6,25 @@ public class PrintAndHide : MonoBehaviour
 {
     int i = 0;
     public Renderer render;
+    int randomNum=0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        randomNum = Random.Range(200, 250);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(gameObject.gameObject.name +" : "+ ++i);
+        Debug.Log(gameObject.name +" : "+ ++i);
+        if(i == 100 && gameObject.tag =="Red")
+        {
+            gameObject.SetActive(false);
+        }
+        if (i== randomNum && gameObject.tag =="Blue")
+        {
+            render.enabled = false;
+        }
     }
 }
